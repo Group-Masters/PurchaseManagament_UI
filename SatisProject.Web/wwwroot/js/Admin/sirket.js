42c70d0e1ad0a73740ad4996646e5460c5da6ceb
Mustafa Yılman <Mustafa Ylman@DESKTOP-R04PVQ3>
2023-11-14 15:49:23 +0300

    Sirket.js updates

--- SatisProject.Web/wwwroot/js/Admin/sirket.js
+++ SatisProject.Web/wwwroot/js/Admin/sirket.js
@@ -39,7 +39,7 @@ function Yeni() {
 function Kaydet() {
     var sirket = {
         Name: $("#inputSirketAd").val(),
-        Adress: $("#inputAdress").val()
+        Address: $("#inputAdress").val()
     };
     Post("Company/Create", sirket, (data) => {
         SirketleriGetir();
@@ -48,7 +48,7 @@ function Kaydet() {
 }
 
 function Sil(id) {
-    Delete(`Company/Delete/${id}`, (data) => {
+    Delete(`Company/DeletePermanent/${id}`, (data) => {
         SirketleriGetir();
     });
 }
