8f6d6e984014c1e9f436631bd83c68abcc2bb4f5
eren ağır <agir_eren@hotmail.com>
2023-11-21 11:44:40 +0300

    raporlarda veriler son eklenene göre listelenme güncellendi

--- SatisProject.Web/wwwroot/js/Raporlar/malzemeRapor.js
+++ SatisProject.Web/wwwroot/js/Raporlar/malzemeRapor.js
@@ -5,7 +5,7 @@
 
     Get(`Report/GetbyProduct/${malzemeSirketId}/${girisId}`, (data) => {
         /*var arr = data;*/
-        var arr = data.sort((a, b) => b.id - a.id);
+        var arr = data.sort((a, b) => b.requestId - a.requestId);
         html += `            <nav class="navbar bg-white mb-2">
               <button type="submit" class="btn btn-warning mr-3" title="PDF Oluştur" id="pdfOlustur" onclick="Pdf(${girisId},${malzemeSirketId})">
                     PDF Oluştur
